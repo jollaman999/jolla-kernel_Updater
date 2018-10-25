cbd26c78bbc835674d3d224523dbf615fb8c9f39
jollaman999 <admin@jollaman999.com>
2018-10-25 19:43:53 +0900

    Version up to v1.12

--- app/src/main/java/com/jollakernelupdater/utils/Config.java
+++ app/src/main/java/com/jollakernelupdater/utils/Config.java
@@ -25,7 +25,7 @@ import java.util.Locale;
 
 public class Config {
 
-    public static final String VERSION = "1.11";
+    public static final String VERSION = "1.12";
 
     public static final String LOG_TAG = "jolla::";
 
